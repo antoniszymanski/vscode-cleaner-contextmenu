e63e94bb8b4d7567237d7f472840b02d112a3680
Antoni Szymański <antoni.szymanski+github@proton.me>
2026-06-20 20:38:57 +0200

    build: migrate from Biome to Oxc
    Related to: https://github.com/biomejs/biome/issues/10640
    Related to: https://github.com/biomejs/biome/discussions/6546

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@
 import menuStyle from "./style.css"
 
 // @ts-expect-error
+// oxlint-disable-next-line typescript/unbound-method
 Element.prototype._attachShadow = Element.prototype.attachShadow
 Element.prototype.attachShadow = function () {
 	// @ts-expect-error
